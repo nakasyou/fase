94c107f7fbb9ffc2fb2b2b62ac17ea3ce6ffd221
nakasyou <79000684+nakasyou@users.noreply.github.com>
2023-01-18 17:10:01 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,19 @@
 class Test{
-  
+  getInfo(){
+    return {
+      id:"test",
+      name:"test",
+      blocks:[
+        {
+          opcode:"test",
+          blockType: Scratch.BlockType.REPORTER,
+          text:"test!"
+        }
+      ]
+    }
+  }
+  test(){
+    return 'test!!'
+  }
 }
+Scratch.extensions.register(new StrictEqualityExtension());
